ac04ba5138412c8d2a4a30c2243ae8adabaa4545
Arfizur Rahman <arfizrahman0@gmail.com>
2019-08-14 14:55:45 +0600

    Implement register action in authController

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repository;
+        public AuthController(IAuthRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+            if(await _repository.UserExists(userForRegisterDto.Username))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _repository.Register(userToCreate, userForRegisterDto.Username);
+
+            return StatusCode(201);
+        }
+    }
+}
