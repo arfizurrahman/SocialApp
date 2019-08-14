b74cc1d1fdbc30017ca399219938e8cf20999d28
Arfizur Rahman <arfizrahman0@gmail.com>
2019-08-14 22:22:05 +0600

    Add token generation procedure in login method

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required(ErrorMessage = "Please enter username")]
         public string Username { get; set; }
+
+        [Required (ErrorMessage = "Please enter password")]
+        [StringLength(10, MinimumLength = 4, ErrorMessage = "Password must be between 4 to 10 characters")]
         public string Password { get; set; }
     }
 }
