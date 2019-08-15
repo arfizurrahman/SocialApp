a6554b7c4dc117ad631dea3763a8fc0fdcf52481
Arfizur Rahman <arfizrahman0@gmail.com>
2019-08-15 12:13:46 +0600

    Handle server and client side errors

---
+++ DatingApp-SPA/src/app/services/error.interceptor.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor{
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(
+            catchError(error => {
+                if (error instanceof HttpErrorResponse) {
+                    if (error.status === 401) {
+                        return throwError(error.statusText);
+                    }
+                    const aplicationError = error.headers.get('Application-Error');
+                    if (aplicationError) {
+                        console.error(aplicationError);
+                        return throwError(aplicationError);
+                    }
+                    const serverError = error.error;
+                    let modelStateErrors = '';
+                    if (serverError && typeof serverError === 'object') {
+                        for (const key in serverError) {
+                            if (serverError[key]) {
+                                modelStateErrors += serverError[key] + '\n';
+                            }
+                        }
+                    }
+
+                    return throwError(modelStateErrors || serverError || 'Server Error');
+                }
+            })
+        );
+    }
+}
+
+
+export const ErrorInterceptorProvider = {
+    provide: HTTP_INTERCEPTORS,
+    useClass: ErrorInterceptor,
+    multi: true
+}
