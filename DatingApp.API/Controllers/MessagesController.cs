dc0541bc368716607b966eb6b4cdb6d70176822b
Arfizur Rahman <arfizrahman0@gmail.com>
2019-08-25 15:55:25 +0600

    Implmenet message creation feature in API

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using DatingApp.API.Helpers;
+using DatingApp.API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -35,6 +38,31 @@ namespace DatingApp.API.Controllers
                 return NotFound();
             
             return Ok(messageFromRepo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
+        {
+            if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            
+            messageForCreationDto.SenderId = userId;
+
+            var recipient = await _repo.GetUser(messageForCreationDto.RecipientId);
+
+            if(recipient == null)
+                return BadRequest("Could not find user");
+
+            var message = _mapper.Map<Message>(messageForCreationDto);
+
+            _repo.Add(message);
+
+            var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
+
+            if(await _repo.SaveAll())
+                return CreatedAtRoute("GetMessage", new { id = message.Id}, messageToReturn);
+            
+            throw new Exception("Creating the message failed on save");
         }
     }
 }
