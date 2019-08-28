c599e628bb3e9363c7d8331a89bd908d450f2ff7
Arfizur Rahman <arfizrahman0@gmail.com>
2019-08-28 14:40:19 +0600

    Add get user roles and edit user role features

--- DatingApp.API/Controllers/AdminController.cs
+++ DatingApp.API/Controllers/AdminController.cs
@@ -1,5 +1,12 @@
+using System.Threading.Tasks;
+using System.Linq;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Dtos;
+using Microsoft.AspNetCore.Identity;
+using DatingApp.API.Models;
 
 namespace DatingApp.API.Controllers
 {
@@ -7,16 +14,64 @@ namespace DatingApp.API.Controllers
     [Route("api/[controller]")]
     public class AdminController : ControllerBase
     {
-        [Authorize(Policy="RequiredAdminRole")]
-        [HttpGet("userWithRoles")]
-        public IActionResult GetUserWithRoles() 
+        private readonly DataContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public AdminController(DataContext context, UserManager<User> userManager)
         {
-            return Ok("Only admin users can see this");
+            _userManager = userManager;
+            _context = context;
+        }
+
+        [Authorize(Policy = "RequiredAdminRole")]
+        [HttpGet("userWithRoles")]
+        public async Task<IActionResult> GetUserWithRoles()
+        {
+            var userList = await (from user in _context.Users
+                                  orderby user.UserName
+                                  select new
+                                  {
+                                      Id = user.Id,
+                                      UserName = user.UserName,
+                                      Roles = (from userRole in user.UserRoles
+                                               join role in _context.Roles
+                                               on userRole.RoleId equals
+                                               role.Id
+                                               select role.Name).ToList()
+                                  }).ToListAsync();
+
+            return Ok(userList);
+        }
+
+        [Authorize(Policy = "RequiredAdminRole")]
+        [HttpPost("editRoles/{userName}")]
+        public async Task<IActionResult> EditRoles(string userName, RoleEditDto roleEditDto)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var selectedRoles = roleEditDto.RoleNames;
+
+            //selectedRoles = selectedRoles != null ? selectedRoles : new string[] {}
+            selectedRoles = selectedRoles ?? new string[] {};
+
+            var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+            if(!result.Succeeded)
+                return BadRequest("Failed to add to roles");
+            
+            result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if(!result.Succeeded)
+                return BadRequest("Failed to remove from roles");
+            
+            return Ok(await _userManager.GetRolesAsync(user));
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
         [HttpGet("photosForModeration")]
-        public IActionResult GetPhotosForModeration() 
+        public IActionResult GetPhotosForModeration()
         {
             return Ok("Only admins and moderators can see this");
         }
